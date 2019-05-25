cd3e40c40041534f39b46f9da165a2d9d9ba06a9
tamim36 <tamimarefinanik@gmail.com>
2019-05-25 17:34:08 +0600

    display data from database

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings' : listings
+    }
+
+    return render(request, 'listings/listings.html',context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
