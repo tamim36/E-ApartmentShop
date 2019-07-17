e8c92da4f9c0e5a47d68cc1778f4acb94d510f8d
tamim36 <tamimarefinanik@gmail.com>
2019-07-18 05:44:30 +0600

    Inquiry Form Fillup & submit

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact( listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
+
+        contact.save()
+
+        return redirect('/listings/' +listing_id)
